3c944f7d59062d653f782cef7fba265386a721e7
Babiduba <subspacebubble@yahoo.com>
2023-01-04 23:37:14 +0100

    http to https fix

--- js/okvideo.js
+++ js/okvideo.js
@@ -1,6 +1,6 @@
 /*
  * OKVideo by OKFocus v2.3.2
- * http://okfoc.us
+ * https://okfoc.us
  *
  * Copyright 2014, OKFocus
  * Licensed under the MIT license.
@@ -100,7 +100,7 @@ var player, OKEvents, options;
 
 			// if necessary, debug with the most recent version of froogaloop
     	// base.insertJS('https://rawgithub.com/vimeo/player-api/master/javascript/froogaloop.js', function(){
-    	// base.insertJS('http://a.vimeocdn.com/js/froogaloop2.min.js', function(){
+    	// base.insertJS('https://a.vimeocdn.com/js/froogaloop2.min.js', function(){
     	base.insertJS('//origin-assets.vimeo.com/js/froogaloop2.min.js', function(){
         vimeoPlayerReady();
       });
